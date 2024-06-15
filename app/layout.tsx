d37e25082fc363ab325a5e93cee1dd3f00b34d7b
NirajMalokar21 <nirajmalokar@gmail.com>
2024-06-15 13:13:10 +0530

    add layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,47 @@ import {
   UserButton
 } from '@clerk/nextjs'
 import './globals.css'
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google'
+import { Metadata } from 'next'
+
+const inter = Inter ({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+const spaceGrotesk = Space_Grotesk ({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: 'A community driven platform for asking asn answering programming questions. Get help, share knowledge, and collaborate with devs.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  }
+}
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: 'primary-text-gradient hover: text-primary-500'
+      }
+    }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
