e2a2f8d2112da97884b86c5fde8639ecebf9fdd8
NirajMalokar21 <nirajmalokar@gmail.com>
2024-06-22 16:06:12 +0530

    Add  ask question page front end

---
+++ components/forms/Question.tsx
@@ -0,0 +1,215 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+"use client"
+import React, { useRef, useState } from 'react'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Editor } from '@tinymce/tinymce-react';
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { questionSchema } from '@/lib/validations'
+import { useTheme } from '@/context/ThemeProvider'
+import { Badge } from '../ui/badge'
+import Image from 'next/image'
+import RenderTag from '../shared/RenderTag'
+
+const type: any = 'create'
+
+const Question = () => {
+  const { mode } = useTheme();
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const editorRef = useRef(null)
+   // 1. Define your form.
+   const form = useForm<z.infer<typeof questionSchema>>({
+    resolver: zodResolver(questionSchema),
+    defaultValues: {
+      title: "",
+      explanation: "",
+      tags: []
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof questionSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    setIsSubmitting(true)
+    try{
+      // Make an async call to your API to create a question
+      // contain all form data
+
+      // navigate to home page
+    } catch(error) {
+      
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field: any) => {
+    if(e.key === 'Enter' && field.name === 'tags'){
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if(tagValue !== ""){
+        if(tagValue.length > 15){
+          return form.setError('tags', {
+            type: 'required',
+            message: 'Tag mustbe less than 15 characters.'
+          })
+        }
+
+        if(!field.value.includes(tagValue as never)){
+          form.setValue('tags', [...field.value, tagValue])
+          tagInput.value = ''
+          form.clearErrors('tags');
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  }
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag)
+
+    form.setValue('tags', newTags)
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col gap-10">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className='flex w-full flex-col'>
+                <FormLabel className='paragraph-semibold text-dark400-light800'>
+                  Question Title <span className="text-primary-500">*</span>
+                </FormLabel>
+                <FormControl className='mt-3.5'>
+                  <Input className='np-focus paragraph-regular background-light700_dark300 light-border-2 
+                  text-dark300_light700 min-h-[56px] border'
+                  {...field} />
+                </FormControl>
+                <FormDescription className='body-regular text-light-500 mt-2.5'>
+                  Be specific and imagine you&apos;re asking a question to another person.
+                </FormDescription>
+                <FormMessage className='text-red-500'/>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="explanation"
+            render={({ field }) => (
+              <FormItem className='flex w-full flex-col gap-3'>
+                <FormLabel className='paragraph-semibold text-dark400-light800'>
+                  Detailed explanation of your problem <span className="text-primary-500">*</span>
+                </FormLabel>
+                <FormControl className='mt-3.5'>
+                  <Editor
+                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                    onInit={(_evt, editor) => editorRef.current = editor}
+                    initialValue="<p>This is the initial content of the editor.</p>"
+                    init={{
+                      height: 350,
+                      menubar: false,
+                      plugins: [
+                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                        'anchor', 'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
+                        'insertdatetime', 'media', 'table', 'wordcount'
+                      ],
+                      toolbar: 'undo redo | blocks | ' +
+                        'codesample | bold italic forecolor | alignleft aligncenter |' +
+                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                        'removeformat | help',
+                      content_style: 'body { font-family:Inter; font-size:16px }',
+                      skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
+                      content_css: mode === 'dark' ? 'dark' : 'light',
+                    }}
+                  />
+                </FormControl>
+                <FormDescription className='body-regular text-light-500 mt-2.5'>
+                  Be specific and imagine you&apos;re asking a question to another person.
+                </FormDescription>
+                <FormMessage className='text-red-500'/>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="tags"
+            render={({ field }) => (
+              <FormItem className='flex w-full flex-col'>
+                <FormLabel className='paragraph-semibold text-dark400-light800'>
+                  Tags <span className="text-primary-500">*</span>
+                </FormLabel>
+                <FormControl className='mt-3.5'>
+                  <>
+                    <Input className='np-focus paragraph-regular background-light900_dark300 light-border-2 
+                    text-dark300_light700 min-h-[56px] border'
+                    placeholder='Add tags...' onKeyDown={(e) => handleInputKeyDown(e, field)}/>
+
+                    {field.value.length > 0 && (
+                      <div className='flex-start mt-2.5 gap-2.5'>
+                        {field.value.map((tag: any) => (
+                          <Badge key={tag}
+                          className='paragraph-regular background-light800_dark300 text-light400_light500 rounded0md flex
+                          items-center justify-center gap-2 border-none px-4 py-2 capitalize'>
+                            {tag}
+                            <Image 
+                              src='assets/icons/close.svg'
+                              alt='Close icon'
+                              width={12}
+                              height={12}
+                              className='cursor-pointer object-contain invert-0 dark:invert'
+                              onClick={() => handleTagRemove(tag, field)}
+                            /> 
+                            
+                          </Badge>
+                           
+                        ))}
+                      </div>
+                    )}
+                  </>
+                </FormControl>
+                <FormDescription className='body-regular text-light-500 mt-2.5'>
+                  Add up to 3 tags to decribe what your question is about.
+                </FormDescription>
+                <FormMessage className='text-red-500'/>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className='primary-gradient !text-light-900 w-fit' 
+          disabled={isSubmitting}>
+            {isSubmitting ? (
+              <>
+                {type === 'edit' ? 'Editing...' : 'Posting'}
+              </>) : ( 
+              <>
+                {type === 'edit' ? 'Edit Question' : 'Ask a Question'}  
+              </>
+            )}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default Question
