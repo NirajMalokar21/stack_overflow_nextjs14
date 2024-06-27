460720c1360d7845698fba9d16831c2c4af53c92
NirajMalokar21 <nirajmalokar@gmail.com>
2024-06-27 16:00:26 +0530

    Add username as email

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -69,14 +69,14 @@ export async function POST(req: Request) {
       return NextResponse.json({ message: 'OK', user: mongoUser})
   }
   else if(eventType === 'user.updated') {
-  const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
+  const { id, email_addresses, image_url, first_name, last_name } = evt.data;
       console.log('Updating user local')
       const mongoUser = await updateUser({
           clerkId: id,
           updateData: {
               name: `${first_name}${last_name ? ` ${last_name} `: ''}`,
               email: email_addresses[0].email_address,
-              username: username!,
+              username: email_addresses[0].email_address,
               picture: image_url
           },
           path: `path/${id}`
