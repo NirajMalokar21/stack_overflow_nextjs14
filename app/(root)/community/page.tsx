8d9b30ba3c3e2ea6e1ce9064c8ca9d2fd5353df7
NirajMalokar21 <nirajmalokar@gmail.com>
2024-06-27 20:01:42 +0530

    Start community page

---
+++ app/(root)/community/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = async () => {
+
+    return (
+      <div>
+        <h1 className='h1-bold text-dark100_light900'>Community</h1>
+        <div>
+          
+        </div>
+      </div>
+    )
+  }
+  
+  export default page
