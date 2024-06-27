470dde75ae3cac3f88f9c83e238c7e1b97c65b64
NirajMalokar21 <nirajmalokar@gmail.com>
2024-06-27 11:25:39 +0530

    Add webhooks to update User db

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,9 @@
 
 import User from "@/database/user.model";
 import { connectToDatabse } from "../mongoose"
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
     try {
@@ -13,5 +16,65 @@ export async function getUserById(params: any) {
 
     } catch(error) {
         console.log(error)
+    }
+}
+
+export async function createUser(userData: CreateUserParams){
+    try {
+        connectToDatabse()
+
+        const newUser = await User.create(userData)
+
+        return newUser;
+
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+export async function updateUser(params: UpdateUserParams){
+    try {
+        connectToDatabse()
+
+        const { clerkId, updateData, path } = params;
+
+        await User.findOneAndUpdate({ clerkId }, updateData, {
+            new: true,
+        })
+
+        revalidatePath(path);
+
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+export async function deleteUser (params: DeleteUserParams){
+    try {
+        connectToDatabse()
+
+        const { clerkId } = params;
+
+        const user = await User.findOneAndDelete({ clerkId });
+
+        if(!user){
+            throw new Error('User not found');
+        }
+
+        // Delete user from database
+        // and question, answers, comments, etc
+        const userQuestionIds = await Question.find({ author: user._id}).distinct('_id')
+
+        await Question.deleteMany({ author: user._id})
+
+        // TODO: delete user answers, comments, etc.
+
+        const deletedUser = await User.findByIdAndDelete({ clerkId })
+
+        return deletedUser
+
+    } catch(error) {
+        console.log(error);
+        throw error;
     }
 }
