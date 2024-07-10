e2dd41e39d9d6dc99197fa83d2f5783876a19083
NirajMalokar21 <nirajmalokar@gmail.com>
2024-07-10 22:31:07 +0530

    Add interaction model and question views

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { downvoteAnswer, upvoteAnswer } from '@/lib/actions/answer.action';
+import { viewQuestion } from '@/lib/actions/interaction.action';
 import { downvoteQuestion, upvoteQuestion } from '@/lib/actions/question.action';
 import { toggleSaveQuestion } from '@/lib/actions/user.action';
 import { formatNumber } from '@/lib/utils';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import React from 'react'
+import { usePathname, useRouter } from 'next/navigation';
+
+import React, { useEffect } from 'react'
 
 interface Props {
     type: string;
@@ -29,6 +31,7 @@ const Votes = ({
     hasSaved
 }: Props) => {
     const pathname = usePathname()
+    const router = useRouter()
     
     const handleSave = async () => {
       await toggleSaveQuestion({
@@ -88,7 +91,14 @@ const Votes = ({
           // todo: show a toast
           
         }
-      }
+    }
+
+    useEffect(() => {
+      viewQuestion({
+        questionId: JSON.parse(itemId),
+        userId: userId ? JSON.parse(userId) : undefined,
+      })
+    }, [itemId, userId, pathname, router])
 
     return (
         <div className="flex items-center">
