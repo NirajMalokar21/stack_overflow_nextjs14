7a1a71a3db2b0a97ced9bce4d5d0aa1ab1f543da
NirajMalokar21 <nirajmalokar@gmail.com>
2024-07-22 13:42:33 +0530

    Add recommendation system for questions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -7,8 +7,9 @@ import Pagination from '@/components/shared/Pagination'
 import LocalSearchBar from '@/components/shared/search/LocalSearchBar'
 import { Button } from '@/components/ui/button'
 import { HomePageFilters } from '@/constants/filters'
-import { getQuestions } from '@/lib/actions/question.action'
+import { getQuestions, getRecommendedQuestions } from '@/lib/actions/question.action'
 import { SearchParamsProps } from '@/types'
+import { auth } from '@clerk/nextjs/server'
 import { Metadata } from 'next'
 import Link from 'next/link'
 import React from 'react'
@@ -18,12 +19,33 @@ export const metadata: Metadata = {
 }
 
 export default async function Home({ searchParams }: SearchParamsProps) {
-  const result = await getQuestions({
-    searchQuery: searchParams.q,
-    filter: searchParams.filter,
-    page: searchParams.page ? +searchParams.page : 1,
-    pageSize: 5
-  });
+  const { userId } = auth()
+  let result;
+
+  if(searchParams?.filter === 'recommended') {
+    if(userId) {
+      result = await getRecommendedQuestions({
+        userId,
+        searchQuery: searchParams.q,
+        page: searchParams?.page ? +searchParams.page : 1,
+      })
+    }
+    else {
+      result = {
+        questions: [],
+        isNext: false
+      }
+    }
+  } else {
+    result  = await getQuestions({
+      searchQuery: searchParams.q,
+      filter: searchParams.filter,
+      page: searchParams.page ? +searchParams.page : 1,
+      pageSize: 5
+    });
+  }
+
+  
 
   return (
     <>
